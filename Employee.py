3aaba4641640bc563051fae58863f16257ec583a
poojangit <poojang147@gmail.com>
2024-08-14 18:26:27 +0530

    [POOJA] Added : Implemented 3rd use case for adding the part-time wage

--- Employee.py
+++ Employee.py
@@ -22,7 +22,7 @@ def welcome_message() :
     print("Welcome to Employee wage computation program")
     
 def generate_random_number() : 
-    return random.randint(0,1)
+    return random.randint(0,2)
 
    
 
@@ -31,17 +31,20 @@ if __name__ == '__main__' :
     
     wagePerHour = 20
     fullDayHour = 8
+    partTimeHour = 4
     
     for employee in empDetails:
         dailyWage = 0
         attendance = generate_random_number()
         if attendance == 0:
             employee['attendance'] = "Absent"
-        else:
-            employee['attendance'] = "Present"
+        elif attendance == 1:
+            employee['attendance'] = "Part-time"
+            dailyWage = wagePerHour * partTimeHour
+        else: 
+            employee['attendance'] = "Full-time"
             dailyWage = wagePerHour * fullDayHour
-            employee['daiyWage'] = dailyWage
             
+        employee['daiyWage'] = dailyWage
         print(f"Daily Wages of {employee['empName']} : {dailyWage}")
-    
         print(employee)

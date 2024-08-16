219e1ad75d7e9cbd878147748d6e809ebdd011fd
poojangit <poojang147@gmail.com>
2024-08-16 12:58:32 +0530

    [POOJA] Added : Imlemented 5th usecase for calculating the wages for a month

--- Employee.py
+++ Employee.py
@@ -18,35 +18,46 @@ empDetails = [
     }
 ]
 
-def welcome_message() :
+def welcome_message():
     print("Welcome to Employee wage computation program")
     
-def generate_random_number() : 
+def generate_random_number(): 
     return random.randint(0,2)
 
-def wage_computation():
+def daily_wage_computation(employee):
     
     wagePerHour = 20
     fullDayHour = 8
     partTimeHour = 4
+    dailyWage = 0
+    
+    attendance = generate_random_number()
+    if attendance == 0:
+        employee['attendance'] = "Absent"
+    elif attendance == 1:
+        employee['attendance'] = "Part-time"
+        dailyWage = wagePerHour * partTimeHour
+    else: 
+        employee['attendance'] = "Full-time"
+        dailyWage = wagePerHour * fullDayHour
+        
+    return dailyWage
+       
+
+def montly_wage_computation(): 
+    
+    workingDays = 20
     
     for employee in empDetails:
-        dailyWage = 0
-        attendance = generate_random_number()
-        if attendance == 0:
-            employee['attendance'] = "Absent"
-        elif attendance == 1:
-            employee['attendance'] = "Part-time"
-            dailyWage = wagePerHour * partTimeHour
-        else: 
-            employee['attendance'] = "Full-time"
-            dailyWage = wagePerHour * fullDayHour
-            
-        employee['daiyWage'] = dailyWage
-        print(f"Daily Wages of {employee['empName']} : {dailyWage}")
+        monthlyWage = 0
+        for day in range(workingDays) :
+            dailyWage = daily_wage_computation(employee)
+            monthlyWage += dailyWage
+        # print(f"Daily Wages of {employee['empName']} : {dailyWage}")
+        print(f"Montly wages of {employee['empName']} for {workingDays} days: {monthlyWage}")
         print(employee)
-
+            
 if __name__ == '__main__' :
     welcome_message()
-    wage_computation()
+    montly_wage_computation()
         

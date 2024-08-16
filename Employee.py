c6d0b8e43999a490e4186942704b7fc660f5ae7e
poojangit <poojang147@gmail.com>
2024-08-16 13:19:47 +0530

    [POOJA] Added : Implemented UC6 for calculating the wages till a condition reaches the given hours and days

--- Employee.py
+++ Employee.py
@@ -30,6 +30,7 @@ def daily_wage_computation(employee):
     fullDayHour = 8
     partTimeHour = 4
     dailyWage = 0
+    hoursWorked = 0
     
     attendance = generate_random_number()
     if attendance == 0:
@@ -41,19 +42,37 @@ def daily_wage_computation(employee):
         employee['attendance'] = "Full-time"
         dailyWage = wagePerHour * fullDayHour
         
-    return dailyWage
+    return dailyWage, hoursWorked
        
 
 def montly_wage_computation(): 
     
     workingDays = 20
+    maxWorkingHours = 100
     
     for employee in empDetails:
         monthlyWage = 0
-        for day in range(workingDays) :
-            dailyWage = daily_wage_computation(employee)
-            monthlyWage += dailyWage
+        totalHoursWorked = 0
+        totalDaysWorked = 0
+        
+        # for day in range(workingDays) :
+        #     dailyWage = daily_wage_computation(employee)
+        #     monthlyWage += dailyWage
         # print(f"Daily Wages of {employee['empName']} : {dailyWage}")
+        
+        while totalDaysWorked < workingDays and totalHoursWorked < maxWorkingHours : 
+            dailyWage, hoursWorked = daily_wage_computation(employee)
+            monthlyWage = monthlyWage + dailyWage
+            totalHoursWorked = totalHoursWorked + hoursWorked
+            totalDaysWorked = totalDaysWorked + 1
+            
+            if totalHoursWorked >= maxWorkingHours:
+                print(f"Reached max working hours of {maxWorkingHours} hours for {employee['empName']}")
+                break
+            if totalDaysWorked >= workingDays:
+                print(f"Reached max working days of {maxWorkingHours} days for {employee['empName']}")
+                break
+            
         print(f"Montly wages of {employee['empName']} for {workingDays} days: {monthlyWage}")
         print(employee)
             
